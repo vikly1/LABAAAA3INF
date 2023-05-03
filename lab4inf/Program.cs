43929181a1b1b6ec22102dfb9fc09deb771bf36e
ohladitel_arbuza <vovabondarenko88@yandex.ru>
2023-05-03 03:09:20 +0300

    ff

--- lab4inf/Program.cs
+++ lab4inf/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System.Diagnostics;
 using System.Linq.Expressions;
 Console.WriteLine("Программа для получения факториала и числа фибоначчи.");
-Console.WriteLine("Введите число больше нуля");
-startt();
-void startt()
+start();
+void start()
 {
+    Console.WriteLine("Введите число больше нуля");
     bool valid = int.TryParse(Console.ReadLine(), out int number);
     if (valid == true && number > 0)
     {
@@ -13,7 +13,7 @@ void startt()
     else
     {
         Console.WriteLine("Некорректный ввод");
-        startt();
+        start();
     }
     void opers()
     {
@@ -25,10 +25,12 @@ void startt()
             case ConsoleKey.D1:
                 Console.WriteLine();
                 Console.WriteLine(Factorio(number));
+                END();
                 break;
             case ConsoleKey.D2:
                 Console.WriteLine();
                 Console.WriteLine(Fibonachi(number));
+                END();
                 break;
             default:
                 opers();
@@ -44,5 +46,28 @@ void startt()
             if (number == 1 || number == 2) return number - 1;
             return Fibonachi(number - 1) + Fibonachi(number - 2);
         }
+    }
+}
+void END()
+{
+    Console.WriteLine("Желаете ли попробовать снова?");
+    Console.WriteLine("1 - начать заново");
+    Console.WriteLine("2 - выйти");
+    var selectend = Console.ReadKey().Key;
+    switch (selectend)
+    {
+        case ConsoleKey.D1:
+            Console.WriteLine();
+            start();
+            break;
+        case ConsoleKey.D2:
+            if (Console.ReadLine() == "2")
+            {
+                return;
+            }
+            break;
+        default:
+            END();
+            break;
     }
 }
